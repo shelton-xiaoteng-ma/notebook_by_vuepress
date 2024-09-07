0e4cfd2addb7116c5d3a78fa91337e5677422c61
shelton-xiaoteng-ma <shelton.xiaoteng.ma@gmail.com>
2024-09-07 13:36:22 +0000

    Deploying to gh-pages from @ shelton-xiaoteng-ma/maxiaoteng-book@d03fed20c710841b253354c9529d44903663f7bd 🚀

---
+++ assets/01_startup.html-4df1a507.js
@@ -0,0 +1,9 @@
+import{_ as a,Y as e,Z as s,a4 as n}from"./framework-957baa9a.js";const i={},l=n(`<h1 id="_01-startup" tabindex="-1"><a class="header-anchor" href="#_01-startup" aria-hidden="true">#</a> 01 startup</h1><ol><li><p>系统环境</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">node</span> <span class="token parameter variable">-v</span>
+<span class="token operator">&gt;&gt;</span> v22.7.0
+
+npx <span class="token parameter variable">-v</span>
+<span class="token operator">&gt;&gt;</span> <span class="token number">10.8</span>.2
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></li><li><p>创建项目</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>npx create-next-app@latest slack
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></li><li><p>启动项目</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token builtin class-name">cd</span> slack
+<span class="token function">pnpm</span> dev
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div></li></ol>`,2),t=[l];function d(r,c){return e(),s("div",null,t)}const o=a(i,[["render",d],["__file","01_startup.html.vue"]]);export{o as default};
